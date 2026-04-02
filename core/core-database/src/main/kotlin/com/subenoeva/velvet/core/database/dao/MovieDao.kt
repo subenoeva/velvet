8c821199bc74674a5633eab5ec89744618e32d62
Pablo <pcostaoubina@gmail.com>
2026-04-02 03:45:39 +0200

    feat(core-database): add getMoviesByCategoryPreview Flow query to MovieDao

--- core/core-database/src/main/kotlin/com/subenoeva/velvet/core/database/dao/MovieDao.kt
+++ core/core-database/src/main/kotlin/com/subenoeva/velvet/core/database/dao/MovieDao.kt
@@ -9,6 +9,7 @@ import com.subenoeva.velvet.core.database.entity.CastEntity
 import com.subenoeva.velvet.core.database.entity.MovieCategoryEntity
 import com.subenoeva.velvet.core.database.entity.MovieDetailEntity
 import com.subenoeva.velvet.core.database.entity.MovieEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MovieDao {
@@ -34,6 +35,15 @@ interface MovieDao {
         ORDER BY movie_categories.page ASC, movie_categories.`order` ASC
     """)
     suspend fun getMoviesByCategoryList(category: String): List<MovieEntity>
+
+    @Query("""
+        SELECT movies.* FROM movies
+        INNER JOIN movie_categories ON movies.id = movie_categories.movieId
+        WHERE movie_categories.category = :category
+        ORDER BY movie_categories.page ASC, movie_categories.`order` ASC
+        LIMIT :limit
+    """)
+    fun getMoviesByCategoryPreview(category: String, limit: Int): Flow<List<MovieEntity>>
 
     @Query("DELETE FROM movie_categories WHERE category = :category")
     suspend fun clearCategoryMovies(category: String)
