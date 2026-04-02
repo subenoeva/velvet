9522deb796a7eacf6e8541558d46db718e6875f3
Pablo <pcostaoubina@gmail.com>
2026-04-02 04:12:06 +0200

    fix(feature-home): update hiltViewModel import and clean up imports
    - Update `hiltViewModel` import to use `androidx.hilt.lifecycle.viewmodel.compose`
    - Remove unused `androidx.compose.foundation.lazy.items` import in `HomeScreen.kt`

--- feature/feature-home/src/main/kotlin/com/subenoeva/velvet/feature/home/HomeScreen.kt
+++ feature/feature-home/src/main/kotlin/com/subenoeva/velvet/feature/home/HomeScreen.kt
@@ -10,12 +10,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.subenoeva.velvet.core.common.presentation.ObserveEvents
 import com.subenoeva.velvet.core.ui.component.ErrorState
